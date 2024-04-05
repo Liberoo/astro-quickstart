92b93739cbff36bc6efd5a35643309960befc123
Kamil <kamil-jamroz@o2.pl>
2024-04-05 19:03:24 +0200

    FEAT: update files

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,49 @@
 import { defineConfig } from 'astro/config';
+import AstroPWA from '@vite-pwa/astro'
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+  integrations: [AstroPWA({
+    mode: 'development',
+    base: '/',
+    scope: '/',
+    includeAssets: ['favicon.svg'],
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Astro PWA',
+      short_name: 'Astro PWA',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+        },
+        {
+          src: 'pwa-512x512.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+    workbox: {
+      navigateFallback: '/',
+      globPatterns: ['**/*.{css,js,html,svg,png,ico,txt}'],
+    },
+    devOptions: {
+      enabled: true,
+      navigateFallbackAllowlist: [/^\//],
+    },
+    experimental: {
+      directoryAndTrailingSlashHandler: true,
+    }
+  })]
+});
